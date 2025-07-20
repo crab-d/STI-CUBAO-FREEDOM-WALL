0282eec22006d88b9a92c1d92ea3386455f63b9c
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-20 13:51:39 +0800

    Fix reply comment bug, re-reference input when submit is called

--- User/Function/ReplyComment.js
+++ User/Function/ReplyComment.js
@@ -1,25 +1,33 @@
-$(document).ready(()=>{
+$(document).ready(()=>{ 
     $('#response').on('click', '.reply-comment', function() {
         let reply_button_id = $(this).closest('.reply-comment').data('comment-id');
         let post_id = $(this).closest('.reply-comment').data('post-id');
         let display_name = $(this).closest('.reply-comment').data('display-name');
 
+        //Initial reference after clicking reply from comment
         let reply_container = $('#reply-container-'+reply_button_id)
-      
-
-        console.log(display_name);
-        
         reply_container.addClass('d-block').removeClass('d-none')
+
         let reply_input = reply_container.find('.reply_input');
         reply_input.val('@' + display_name + ' ');
 
+        console.log(display_name);
+        console.log(reply_button_id);
+        console.log(reply_input);
 
-        $('#response').off('submit', '#reply-form'+reply_button_id).on('submit','#reply-form-'+reply_button_id , function(e){
+        $('#response').off('submit', '#reply-form-'+reply_button_id).on('submit','#reply-form-'+reply_button_id , function(e){
             e.preventDefault();
+
+            //Since reply isnt being triggered, reference the input when user submit again
+            let reply_container = $('#reply-container-'+reply_button_id)
+            let reply_input = reply_container.find('.reply_input');
             
             if (reply_input.val().trim() !== ''){
                 sendReplyComment(reply_button_id, reply_input.val(), post_id)
-        }
+                reply_input.val('')
+            } else {
+                return
+            }
         })
     })
 
@@ -33,7 +41,6 @@ $(document).ready(()=>{
                     reply_content: reply_input
                 },
                 success: function(response){
-                    console.log(response)
                     loadComments(post_id,reply_button_id)
                 },
                 error: function(){
@@ -54,7 +61,7 @@ $(document).ready(()=>{
             },
             success: function(response) {
                 $comment_container.html(response.User_Comment) 
-        let reply_container = $('#reply-container-'+reply_button_id)
+                let reply_container = $('#reply-container-'+reply_button_id)
 
                 setTimeout(()=>{
                 reply_container.addClass('d-block').removeClass('d-none')

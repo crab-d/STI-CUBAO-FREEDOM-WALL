e6f1798f7a6893c2a3ee95b630a7e674d95aded9
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-21 01:13:09 +0800

    add notice label

--- User/UserProfile.php
+++ User/UserProfile.php
@@ -2,6 +2,9 @@
 include '../User/Components/UserMetaData.php';
 ?>
 <body>
-
+    <div class="vh-100 vw-100 flex-column d-flex justify-content-center align-items-center">
+        <span><i class="bi bi-wrench-adjustable-circle-fill" style="font-size: 10rem;"></i></span>
+        <p class="fs-3 fw-bold poppins-medium">This page is under developement</p>
+    </div>
 </body>
 </html>

c4fc6115de7efba09389bf265f4f7b66178a6e82
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-23 03:37:58 +0800

    delete post

---
+++ User/Function/deletePost.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+  $("#response").on("click", ".delete_post_btn", function () {
+    $post_id = $(this).closest(".user_post").data("post-id");
+    $.ajax({
+      url: "../User/Handler/HidePost.php",
+      type: "POST",
+      data: {
+        post_id: $post_id
+      },
+      success: function () {
+        location.reload()
+      },
+    });
+  });
+});

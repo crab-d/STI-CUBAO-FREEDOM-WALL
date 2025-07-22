52456305f8d8751423b6687a29fa910e3b9f734d
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-23 03:40:25 +0800

    change comment bacckground color to gray

--- User/Handler/RetrieveComment.php
+++ User/Handler/RetrieveComment.php
@@ -22,7 +22,7 @@ while ($row = mysqli_fetch_assoc($result)) {
                     <div style="background-color:' . $_SESSION['profile'] .';height: 15px; width: 15px;" class="rounded-circle me-2  flex-shrink-0"></div>
                     <div class="d-flex flex-column overflow-hidden w-100 text-wrap align-items-start justify-content-end " >
                         <p class=" m-0 text-start" style="font-size: 10px"> '. getUserDisplayName($row['account_id']) .' </p>
-                        <p class="dark-color text-start m-0 rounded p-1 text-white text-wrap" style="font-size:14px; max-width: 90%; overflow-wrap: break-word;" >'. $row['comment_content'] .'</p>
+                        <p class="gray-color text-start m-0 rounded-2 primary-fs p-2 text-dark-subtle text-wrap" style=" max-width: 90%; overflow-wrap: break-word;" >'. $row['comment_content'] .'</p>
                     </div>
                 </div>
                 <div class="d-flex ms-4 mb-0">
@@ -30,12 +30,13 @@ while ($row = mysqli_fetch_assoc($result)) {
                     <p data-comment-id="'. $row['comment_id'].'" data-post-id="'. $row['post_id'].'" data-display-name="'.getUserDisplayName($row['account_id']).'" class="reply-comment primary-text p-0 m-0 ms-2" style="cursor: pointer; font-size: 9px">Reply <span class="  p-1" style="height: 10px; width: 10px; font-size: 9px">'. getPostReplyCount($row['post_id'], $row['comment_id']). '</span></p> 
                 </div>
             </div>
-                    <div id="reply-container-'.$row['comment_id'].'" class="w-100 d-none bg-light d-flex flex-column align-items-start justify-content-start ps-3 p-2">
+                    <div id="reply-container-'.$row['comment_id'].'" class="w-100 d-none d-flex flex-column align-items-start justify-content-start ps-4 p-2">
                     '. getPostCommentReply($row['post_id'], $row['comment_id']) .'
+
                     <div class="w-100 p-0">
                     <form id="reply-form-'. $row['comment_id'].'" class="d-flex w-100 justify-content-between p-2" >
                     <input type="hidden" name="post_id" value="'. $row["post_id"] .'">
-                        <input  id="reply_input"  class="border primary-fs  reply_input p-1 w-100 m-0 comment-input-box rounded border-0 bg-white shadow-sm" placeholder="Comment as ' . getUserDisplayName($account_id) . '" autocomplete="off">
+                        <input  id="reply_input"  class="border primary-fs  reply_input p-1 w-100 m-0 comment-input-box rounded border-0 bg-light shadow-sm" placeholder="Comment as ' . getUserDisplayName($account_id) . '" autocomplete="off">
                         <button   id="reply_submit" class="btn primary-color primary-fs text-white rounded shadow-sm m-1"  type="submit">Send</button>
                     </form>
                     </div>
@@ -77,7 +78,7 @@ function getPostCommentReply($post_id, $comment_id) {
 
                     <div class="d-flex flex-column align-items-start justify-content-end" style="max-width: 90%;" >
                         <p class=" m-0 text-start " style="font-size: 10px"> '. getUserDisplayName($row['account_id']) .' </p>
-                        <p class="dark-color text-start m-0 rounded p-1 text-white" style="font-size: 12px; max-width: 100%; overflow-wrap: break-word;" >'. $row['reply_content'] .'</p>
+                        <p class=" gray-color text-start m-0 rounded-2 p-2 text-dark-subtle" style="font-size: 12px; max-width: 100%; overflow-wrap: break-word;" >'. $row['reply_content'] .'</p>
                     </div>
                 </div>
             </div>

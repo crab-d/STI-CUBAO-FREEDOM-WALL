c14ec29910042bea986833ada165ecf6531b8f37
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-23 01:05:13 +0800

    query user information instead of storing them in table so if user change profile it display updated data

--- User/Handler/RetrieveMessage.php
+++ User/Handler/RetrieveMessage.php
@@ -45,8 +45,8 @@ function getMessages($conn, $defaultActiveValue, $lastChatId, $accountId) {
     while ($row = mysqli_fetch_assoc($result)) {
         $chatId = htmlspecialchars($row['chat_id']);
         $chatContent = htmlspecialchars($row['chat_content']);
-        $displayName = htmlspecialchars($row['display_name']);
 
+        
         if ($row['account_id'] == $accountId) {
         $content .= '
             <div id="CardMessage" data-chat-id="' . $chatId . '" class="chat-message d-flex justify-content-end mt-3">
@@ -55,11 +55,13 @@ function getMessages($conn, $defaultActiveValue, $lastChatId, $accountId) {
                 </p>
             </div>';
         } else {
+        $sender = getUserProfile($row['account_id']);
+
         $content .= '
             <div id="CardMessage" data-chat-id="' . $chatId . '" class="d-flex w-100 mt-3 chat-message" style="max-width: 80%">
-                <div class="primary-color rounded-circle align-self-end me-1 flex-shrink-0" style="height: 15px; width: 15px;"></div>
+                <div class="rounded-circle align-self-end me-1 flex-shrink-0" style="background-color: '. $sender['sender_profile_color'] .'; height: 15px; width: 15px;"></div>
                 <div class="d-flex flex-column gap-0" style="max-width:100%">
-                    <p class="primary-fs m-0"><small>' . $displayName . '</small></p>
+                    <p class="primary-fs m-0"><small>' . $sender['sender_display_name'] . '</small></p>
                     <p class="p-0 m-0 primary-color primary-fs w-auto p-2 flex-shrink-0 text-white rounded" style=" overflow-wrap: break-word" >
                         ' . $chatContent . '
                     </p>
@@ -69,6 +71,29 @@ function getMessages($conn, $defaultActiveValue, $lastChatId, $accountId) {
     }
 
     return $content;
+}
+
+function getUserProfile($accountId) {
+    include('../../Database/db_connect.php');
+
+    $query = 'SELECT display_name, profile_color FROM accounts WHERE account_id = ?';
+    $stmt = mysqli_prepare($conn_accounts, $query);
+    mysqli_stmt_bind_param($stmt, 'i', $accountId);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if ($row = mysqli_fetch_assoc($result)) {
+        $sender_display_name = $row['display_name'];
+        $sender_profile_color = $row['profile_color'];
+        return [
+            'sender_display_name' => $sender_display_name,
+            'sender_profile_color' => $sender_profile_color
+        ];
+    }
+
+    return [
+            'sender_display_name' => "unknown",
+            'sender_profile_color' => "#ff4444"
+        ];
 }
 
 // 1st try: check immediately

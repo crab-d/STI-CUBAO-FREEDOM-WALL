abfa95db32afe6c8a4145b243f2e996a200f2a75
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-24 20:52:12 +0800

    update query, remove display name in table structure

--- User/Handler/SendMessage.php
+++ User/Handler/SendMessage.php
@@ -3,16 +3,15 @@ session_start();
 include '../../Database/db_connect.php';
 
 $accountId = $_SESSION['account_id'];
-$displayName = $_SESSION['display_name'];
 
 date_default_timezone_set("Asia/Manila");
 $chat_date = date('F j, Y g:i A');
 
 if ($_SERVER["REQUEST_METHOD"] === "POST" && !empty($_POST['chatContent'])) {
     $chatContent = htmlspecialchars($_POST['chatContent']);
-    $query = 'INSERT INTO public_chat(account_id, display_name,chat_content,chat_date) VALUES (?,?,?,?)';
+    $query = 'INSERT INTO public_chat(account_id,chat_content,chat_date) VALUES (?,?,?)';
     $stmt = mysqli_prepare($conn_contents, $query);
-    mysqli_stmt_bind_param($stmt, 'isss', $accountId, $displayName, $chatContent, $chat_date);
+    mysqli_stmt_bind_param($stmt, 'iss', $accountId, $chatContent, $chat_date);
     if (mysqli_stmt_execute($stmt)) {
        
     } else {

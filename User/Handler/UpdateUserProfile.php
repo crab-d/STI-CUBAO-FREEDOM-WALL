b3a9bf51dd0beaa4f2f4a38975736f4e581db7d8
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-24 20:51:32 +0800

    reupdate logic only change session if ssuccesful update user data

--- User/Handler/UpdateUserProfile.php
+++ User/Handler/UpdateUserProfile.php
@@ -1,24 +1,31 @@
-<?php
-include('../../Database/db_connect.php');
-session_start();
+    <?php
+    include('../../Database/db_connect.php');
+    session_start();
 
 
-$profile_color = $_POST['profile_color'] ?? '#111111';
-$display_name = $_POST['display_name'] ?? 'error';
+    $profile_color = $_POST['profile_color'] ?? '#111111';
+    $display_name = $_POST['display_name'] ?? 'error';
 
-$_SESSION['profile'] = $profile_color;
-$_SESSION['display_name'] = $display_name;
 
-$account_id = $_SESSION['account_id'];
 
-$query = 'UPDATE accounts SET profile_color = ?, display_name = ? WHERE account_id = ?';
-$stmt = mysqli_prepare($conn_accounts, $query);
-mysqli_stmt_bind_param($stmt, 'ssi', $profile_color, $display_name, $account_id);
-if (mysqli_stmt_execute($stmt)) {
-    echo 'success';
-} else {
-    echo 'failed';
-}
+    $account_id = $_SESSION['account_id'];
 
-print_r($_SESSION)
-?>
+    $stmt = mysqli_prepare($conn_accounts, 'SELECT `account_id` FROM accounts WHERE display_name = ? AND account_id != ?');
+    mysqli_stmt_bind_param($stmt, "si", $display_name, $account_id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if ($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="alert alert-danger">Display name already taken, please try different one</div>';
+    } else {
+        $query = 'UPDATE accounts SET profile_color = ?, display_name = ? WHERE account_id = ?';
+        $stmt = mysqli_prepare($conn_accounts, $query);
+        mysqli_stmt_bind_param($stmt, 'ssi', $profile_color, $display_name, $account_id);
+        if (mysqli_stmt_execute($stmt)) {
+            echo 'success';
+            $_SESSION['profile'] = $profile_color;
+            $_SESSION['display_name'] = $display_name;
+        } else {
+            echo 'failed';                                                                                                                                                                   
+        }
+    }
+    ?>

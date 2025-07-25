ea9959b2e02a8e4272407dd03b64dd10a68e7159
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-07-25 23:23:05 +0800

    initial commit

---
+++ Admin/AdminDashboard.php
@@ -0,0 +1,18 @@
+<?php include('../Admin/Components/AdminMetaData.php') ?>
+
+<body>
+    <div class="container-fluid vh-100 d-flex p-0 m-0 row">
+        <div class="col-2 d-flex flex-column bg-light p-2">
+            <div class="d-flex align-content-center align-items-center p-0 mb-3">
+                <p class="m-0 primary-text poppins-medium fw-bold">SCFW Admin</p>
+                <small class="fw-bold opacity-50 ms-2 m-0 fst-italic text-dark" style="font-size: 7px;">Beta v.2.3</small>
+            </div>
+            <div class="p-1">
+                
+                <button class="w-100 btn text-white text-start p-1 primary-color"><i class="bi bi-person-circle mx-2 text-white"></i>User Dashboard</button>
+            </div>
+        </div>
+        <div class="col-10 bg-danger p-2"></div>
+    </div>
+</body>
+</html>

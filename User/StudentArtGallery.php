0f0c5289f8728bec602ff03f89cc10a517fb9892
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-08-02 23:55:56 +0800

    Cheat commit

--- User/StudentArtGallery.php
+++ User/StudentArtGallery.php
@@ -5,6 +5,7 @@ include '../User/Components/UserMetaData.php';
     <div class="vh-100 vw-100 flex-column d-flex justify-content-center align-items-center">
         <span><i class="bi bi-wrench-adjustable-circle-fill" style="font-size: 3rem;"></i></span>
         <p class="fs-4 fw-bold poppins-medium text-center">This page is under developement</p>
+        <P></P>
     </div>
 </body>
 </html>
